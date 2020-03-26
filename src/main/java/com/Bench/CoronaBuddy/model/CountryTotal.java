68bf487168e50e9810245e3064c9fa706cc948ca
Zach Burmaster <zach.burmaster@S05087-MBPR.local>
2020-03-26 16:50:29 -0500

    wip, not turning json into java object CountryTotal

---
+++ src/main/java/com/Bench/CoronaBuddy/model/CountryTotal.java
@@ -0,0 +1,4 @@
+package com.Bench.CoronaBuddy.model;
+
+public class CountryTotal {
+}
---
+++ src/main/java/com/Bench/CoronaBuddy/model/CountryTotalByDate.java
@@ -0,0 +1,8 @@
+package com.Bench.CoronaBuddy.model;
+
+import java.util.List;
+
+public class CountryTotal {
+
+    private List<>
+}
